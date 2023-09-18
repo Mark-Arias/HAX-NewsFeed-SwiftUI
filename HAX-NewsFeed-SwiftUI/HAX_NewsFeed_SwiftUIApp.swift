2948451ced78373868122ebd47251ee1d853c26d
Mark <mark.arias7@gmail.com>
2023-09-17 22:37:32 -0500

    Initial Commit

---
+++ HAX-NewsFeed-SwiftUI/HAX_NewsFeed_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAX_NewsFeed_SwiftUIApp.swift
+//  HAX-NewsFeed-SwiftUI
+//
+//  Created by Mark Arias on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HAX_NewsFeed_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
